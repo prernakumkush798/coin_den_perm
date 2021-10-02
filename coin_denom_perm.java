6c0b89c3e264b8ed914cbe94dc73d99250676893
AnkitKumar989 <91775514+AnkitKumar989@users.noreply.github.com>
2021-10-02 13:10:40 +0530

    Add files via upload

---
+++ coin_denom_perm.java
@@ -0,0 +1,18 @@
+package lecture_27;
+//permutation ,where supply of each coin is infinite and unique coins are provided
+public class coin_denom_perm {
+	public static void main(String[] args) {
+		int coin[]= {2,3,5,6};
+		coin_den_perm(coin, 10,  "");
+		
+	}
+public static void coin_den_perm(int coin[],int total_amount,String ans) {
+	if(total_amount==0) {
+		System.out.println(ans);
+		return;
+	}
+	 
+	for(int i=0;i<coin.length;i++) {
+	coin_den_perm(coin, total_amount-coin[i], ans+coin[i]);
+}}
+}
